7d203f63ee9821670ffcd61e23f0b0e36a8563c0
Ellie Sceeles <esceeles@gmail.com>
2019-05-24 05:08:57 -0700

    generating random graphs with networkx

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import numpy as np
 import networkx as nx
+import random
 
 # generates random graph with three vertices as adjacency matrix
-adjacency = np.random.randint(0,2,(3,3))
-print(adjacency)
+#adjacency = np.random.randint(0,2,(3,3))
+#print(adjacency)
 
 # generates random graph with (n nodes, m edges)
-er = nx.gnm_random_graph(3, 3, seed=None, directed=False)
-er2 = nx.gnm_random_graph(3, 2, seed=None, directed=False)
+G = nx.gnm_random_graph(3, 3, seed=None, directed=False)
+#er = nx.gnm_random_graph(3, 2, seed=None, directed=False)
 
-G = nx.Graph()
-G.add_edge(1, 2)
+#print(er.adj)
+print(G.adj)
 
-print(er.adj)
-print(er2.adj)
+for (u, v) in G.edges():
+    G.edges[u,v]['weight'] = random.randint(0,10)
+
+print(G.adj)
+
+
+
+
+
+nx.prim_mst_edges(G, True, weight='weight',
+                   keys=True, data=True, ignore_nan=False)
+
 print(G.adj)
